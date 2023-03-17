00ce3544611b5d6fd5466b708f2a925747c7159e
LBissette <loganbissette@gmail.com>
2023-03-17 17:48:44 -0400

    fixed htmlroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,3 +3,10 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        res.json(JSON.parse(data))
+    })
+});
+
+module.exports = router;
