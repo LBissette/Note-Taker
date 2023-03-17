1eb0b3706cfed823dc02245ab8b4be8d59612aa8
LBissette <loganbissette@gmail.com>
2023-03-17 18:25:28 -0400

    created apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,4 +9,51 @@ router.get('/notes', (req, res) => {
     })
 });
 
+router.post('/notes', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+        readAndAppend(newNote, './db/db.json');
+    } else{
+        res.errored('Error in adding note')
+    }
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data)=> {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNote = JSON.parse(data);
+            parsedNote.push(content);
+            fs.writeFile(file, JSON.stringify(parsedNote), (err) => {
+                if (err) throw err;
+                console.log('Note created!')
+            });
+        }
+    });
+};
+
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile('./db/db.json', 'utf-8', (err,data) => {
+        if (err) throw err
+        const parsedData = JSON.parse(data);
+        const newDb = parsedData.filter((item) => item.id !== noteId)
+        fs.writeFile('./db/db.json', JSON.stringify(newDb), (err) => {
+            if (err) throw err;
+            console.log('Note deleted successfully!')
+        });
+    })
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const uuid = require('./helpers/uuid');
 const apiRoutes = require('./routes/apiRoutes')
 const htmlRoutes = require('./routes/htmlRoutes');
 
